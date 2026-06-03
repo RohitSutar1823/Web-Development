248d410b640df32d6c9f360c3c15dccf9a072908
Rohit Sutar <rohit1823sutar@gmail.com>
2026-06-03 12:40:38 +0530

    Learn and Implement JavaScript Variables, Data Types & Objects

---
+++ Javascript/Javascript Variables,Data Types and Objects/script.js
@@ -0,0 +1,34 @@
+console.log("Hey this is javascript")
+
+let a = 5;
+let b =6;
+let c = "Harry";
+// console.log(a+b+8)
+// console.log(typeof a,typeof b,typeof c)
+{
+    let a = 66;
+    console.log(a)
+}
+
+console.log(a)
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name:"Harry",
+    "job code":5600,
+    "is_handsome":true
+}
+console.log(o)
+o.salary = "100crores"
+console.log(o)
+o.salary = "500crores"
+console.log(o)
