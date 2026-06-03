377c97707d5dca940696239e6464762b6ddb6fa1
Rohit Sutar <rohit1823sutar@gmail.com>
2026-06-03 13:30:05 +0530

    Learn and Implement JavaScript Conditionals: if, else if, else ladder

---
+++ Javascript/Javascript Conditionals/index.js
@@ -0,0 +1,21 @@
+console.log("This is conditional tutorial")
+
+let age = 20;
+// let grace = 2;
+// age = age + grace;c
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+
+
+if(age>18)
+{
+    console.log("You can drive")
+}
+
+else{
+    console.log("Youn cannot drive")
+}
