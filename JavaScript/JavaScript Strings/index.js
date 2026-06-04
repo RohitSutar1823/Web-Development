8159477d46c2d60ce574fd989e51118b5808fb62
Rohit Sutar <rohit1823sutar@gmail.com>
2026-06-04 13:29:29 +0530

    Learn and Implement JavaScript Strings

---
+++ JavaScript/JavaScript Strings/index.js
@@ -0,0 +1,25 @@
+console.log("This is a tutorial on Strings");
+
+let a = "Harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a.length);
+
+let real_name = "Harry"
+let friend = "Rohan"
+console.log("The real name is " + real_name + " and his friend is " + friend)
+console.log(`The real name is ${real_name} and his friend name is ${friend}`)
+
+let b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+
+console.log(b.replace("Sh","77"))
+console.log(b.concat(a,"Aishwarya","Rahul","Priya"))
+console.log(b)
