7cef1d32cd153dbebf3ca27242247f127fb4f73c
Rohit Sutar <rohit1823sutar@gmail.com>
2026-06-05 14:49:24 +0530

    Learn and Implement JavaScript DOM - Children, Parent & Sibling Nodes

---
+++ JavaScript/JavaScript DOM-Children, Parent and Sibling Nodes/script.js
@@ -0,0 +1,5 @@
+console.log("Hello World")
+
+console.log(document.body.firstElementChild)
+console.log(document.body.firstElementChild.childNodes)
+console.log(document.body.firstElementChild.children)
