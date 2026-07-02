22a35e8c04f2fbebfbb6abc6dcde43e9aa9741fb
Rohit Sutar <rohit1823sutar@gmail.com>
2026-07-02 18:08:57 +0530

    Learn and Implement Working with Files: fs and path Modules

---
+++ NodeJS/Working with Files - fs and path Modules/aboutpath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let myPath = "C:\\Users\DELL\\OneDrive\\Desktop\\Web Development\\NodeJS\\Working with Files - fs and path Modules\\harry.txt"
+
+console.log(path.extname(myPath))
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/","programs\\harry.txt"))
